ee0fd275dd213f4e3095e2845f053e0a474f74c7
radiosmersh <radiosmersh@gmail.com>
2020-07-17 21:21:12 +0300

    Partially revert last commit

--- source/fontpage.cpp
+++ source/fontpage.cpp
@@ -183,9 +183,9 @@ int CFontPage::AddChar(CFontChar *ch, int channel)
 				}
 			}
 
-			// [radiosmersh] Round up to the nearest number divisible by 4
-			currX = (currX + 4) & ~3;
-			cy = (cy + 4) & ~3;
+			// [TBob] Round up to even
+			currX = (currX + 1) & ~1;
+			cy = (cy + 1) & ~1;
 
 			AddChar(currX, cy, ch, channel);
 
